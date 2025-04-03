765e0a5d0ebfc060ff7cf475f93fa2f041d035cd
Federico Quintieri <fedequinti@gmail.com>
2025-04-03 14:20:43 +0200

    Fragments polishing

--- src/main/java/org/java/pizzeria/spring_la_mia_pizzeria_crud/controller/HomeController.java
+++ src/main/java/org/java/pizzeria/spring_la_mia_pizzeria_crud/controller/HomeController.java
@@ -1,7 +1,17 @@
 package org.java.pizzeria.spring_la_mia_pizzeria_crud.controller;
 
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("/")
 public class HomeController {
-    public HomeController() {
-        // TODO Auto-generated method stub
+
+    @GetMapping()
+    public String home() {
+
+        return "home";
     }
+
 }
--- src/main/java/org/java/pizzeria/spring_la_mia_pizzeria_crud/controller/PizzaController.java
+++ src/main/java/org/java/pizzeria/spring_la_mia_pizzeria_crud/controller/PizzaController.java
@@ -8,6 +8,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
@@ -20,10 +21,16 @@ public class PizzaController {
     @GetMapping
     public String index(Model model) {
         List<Pizza> pizze = repository.findAll();
-
         model.addAttribute("pizze", pizze);
-
         return "pizze/index";
+    }
+
+    @GetMapping("/{id}")
+    public String show(@PathVariable("id") Integer id, Model model) {
+        // Trovo solo la row in base all'id e la metto nell'attributo pizza
+        model.addAttribute("pizza", repository.findById(id).get());
+
+        return "pizze/show";
     }
 
 }
